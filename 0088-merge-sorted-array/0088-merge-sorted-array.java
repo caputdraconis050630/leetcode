c73f19ff1a7bf015f38302987f8e8a0181448369
caputdraconis <caputdraconis@kakao.com>
2022-11-11 14:53:44 +0900

    Time: 0 ms (100.00%), Space: 43.3 MB (12.84%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,11 +1,13 @@
-import java.util.Arrays;
-
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int nums1_len = nums1.length;
-        for (int i=m; i < nums1_len; i++) {
-            nums1[i] = nums2[--n];
+        int p1 = m-1 , p2 = n-1 ,i = m+n-1;
+        while(p2 >=0 ){
+            if(p1 >=0 && nums1[p1] > nums2[p2]){
+                nums1[i--] = nums1[p1--];
+            } 
+            else{
+                nums1[i--] = nums2[p2--];
+            }
         }
-        Arrays.sort(nums1);
-    }
+       }
 }
