f018cee207405527fd39104f72ccaed9efcf899f
caputdraconis <caputdraconis@kakao.com>
2022-11-17 09:43:52 +0900

    Time: 0 ms (100.00%), Space: 42.1 MB (83.20%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -10,11 +10,10 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null) {
+        if (head == null || head.next == null)
             return head;
-        }
         ListNode cur = head;
-        while (cur.next != null) {
+        while (cur != null && cur.next != null) {
             if (cur.val == cur.next.val) {
                 cur.next = cur.next.next;
             } else {
