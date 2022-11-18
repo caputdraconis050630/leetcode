7b40a2ab6cab6f06eef7ab188f0e48cd3d0d5d5d
caputdraconis <caputdraconis@kakao.com>
2022-11-18 09:41:33 +0900

    Time: 4 ms (58.10%), Space: 42 MB (48.55%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean isValid(String s) {
-        char[] chars = s.toCharArray();
-        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray(); // String을 Char Array 타입으로(for 문으로 돌기 편하게)
+        Stack<Character> stack = new Stack<>(); // '(' || '[' || '{'이 들어왔을 때 push할 스택
         for (char element : chars) {
             if (element == '(' || element == '[' || element == '{') {
                 stack.push(element);
                 continue;
             } else if (stack.empty()) {
+                // ')' || ']' || '}' 이 들어왔을 때
                 return false;
             }
             char top = stack.pop();
