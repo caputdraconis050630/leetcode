8b6b332deca2d87ee446e0fe84fa64e541f3e2d5
caputdraconis <caputdraconis@kakao.com>
2022-11-24 21:37:26 +0900

    Time: 347 ms (31.97%), Space: 16.7 MB (51.32%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,5 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        # XOR 사용
+        t = 0
         for i in nums:
-            if nums.count(i) == 1:
-                return i
+            t ^= i
+        return t
